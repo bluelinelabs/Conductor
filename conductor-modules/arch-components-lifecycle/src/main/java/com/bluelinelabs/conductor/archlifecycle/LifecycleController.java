cf05b858b0bf78465dc09a8e604c002c99f6c4ad
Gustavo Pagani <luizgrp@gmail.com>
2017-07-18 20:57:34 +0100

    Expose constructor with Bundle as parameter

--- conductor-modules/arch-components-lifecycle/src/main/java/com/bluelinelabs/conductor/archlifecycle/LifecycleController.java
+++ conductor-modules/arch-components-lifecycle/src/main/java/com/bluelinelabs/conductor/archlifecycle/LifecycleController.java
@@ -2,12 +2,22 @@ package com.bluelinelabs.conductor.archlifecycle;
 
 import android.arch.lifecycle.LifecycleRegistry;
 import android.arch.lifecycle.LifecycleRegistryOwner;
+import android.os.Bundle;
 
 import com.bluelinelabs.conductor.Controller;
 
 public abstract class LifecycleController extends Controller implements LifecycleRegistryOwner {
 
-    private final ControllerLifecycleRegistryOwner lifecycleRegistryOwner = new ControllerLifecycleRegistryOwner(this);
+    private final ControllerLifecycleRegistryOwner lifecycleRegistryOwner;
+
+    public LifecycleController() {
+        this(null);
+    }
+
+    public LifecycleController(Bundle args) {
+        super(args);
+        lifecycleRegistryOwner = new ControllerLifecycleRegistryOwner(this);
+    }
 
     @Override
     public LifecycleRegistry getLifecycle() {
