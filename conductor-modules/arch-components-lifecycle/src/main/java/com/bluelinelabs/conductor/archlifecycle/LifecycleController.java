480db2290fce3a4dfc851dfd6d16301cb01a37b9
Gustavo Pagani <luizgrp@gmail.com>
2017-07-21 09:45:45 +0100

    Add Nullable annotation to Bundle parameter

--- conductor-modules/arch-components-lifecycle/src/main/java/com/bluelinelabs/conductor/archlifecycle/LifecycleController.java
+++ conductor-modules/arch-components-lifecycle/src/main/java/com/bluelinelabs/conductor/archlifecycle/LifecycleController.java
@@ -3,20 +3,20 @@ package com.bluelinelabs.conductor.archlifecycle;
 import android.arch.lifecycle.LifecycleRegistry;
 import android.arch.lifecycle.LifecycleRegistryOwner;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 
 import com.bluelinelabs.conductor.Controller;
 
 public abstract class LifecycleController extends Controller implements LifecycleRegistryOwner {
 
-    private final ControllerLifecycleRegistryOwner lifecycleRegistryOwner;
+    private final ControllerLifecycleRegistryOwner lifecycleRegistryOwner = new ControllerLifecycleRegistryOwner(this);
 
     public LifecycleController() {
-        this(null);
+        super();
     }
 
-    public LifecycleController(Bundle args) {
+    public LifecycleController(@Nullable Bundle args) {
         super(args);
-        lifecycleRegistryOwner = new ControllerLifecycleRegistryOwner(this);
     }
 
     @Override
